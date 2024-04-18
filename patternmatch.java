8b87a17b050e96b22ad26e75e0d12b31dd5e01ee
Gaurav <gauravbhagat326@gmail.com>
2024-04-18 23:15:06 +0530

    word pattern matching

---
+++ patternmatch.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class patternmatch {
+    public static void solution(String str, String pattern, HashMap<Character,String> map, String op) {
+        if (pattern.length() == 0) {
+            if (str.length() == 0) {
+                HashSet<Character> alreadyprinted = new HashSet<>();
+                for (int i = 0; i < op.length(); i++) {
+                    char ch = op.charAt(i);
+                    if (alreadyprinted.contains(ch)) {
+                      // alreadyprinted.add(ch);
+                        System.out.print(ch + " -> " + map.get(ch) + ", ");
+                        alreadyprinted.add(ch);
+                    }
+                }
+                System.out.println(".");
+            }
+            return;
+        }
+        char ch = pattern.charAt(0);
+        String rop = pattern.substring(1);
+        if (map.containsKey(ch)) {
+            String previousmapping = map.get(ch);
+            if (str.length() >= previousmapping.length()) {
+                String left = str.substring(0, previousmapping.length());
+                String right = str.substring(previousmapping.length());
+                if (previousmapping.equals(left)) {
+                    solution(right, rop, map, op);
+                }
+            }
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String left = str.substring(0, i + 1);
+                String right = str.substring(i + 1);
+                map.put(ch, left);
+                solution(right, rop, map,op );
+                map.remove(ch);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        String pattern = scn.next();
+        HashMap<Character,String> map = new HashMap<>();
+        //HashMap<String,Character> map2 = new HashMap<>();
+        solution(str,pattern,map,pattern);
+    }
+
+    
+}
